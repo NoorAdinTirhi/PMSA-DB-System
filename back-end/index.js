612e5bf5423b876d117c905303a7f72693a13dbf
NoorAdinTirhi <nooradintirhi@gmail.com>
2023-09-02 16:49:10 +0300

    modified login using ejs instead of html

--- back-end/index.js
+++ back-end/index.js
@@ -5,24 +5,24 @@ const bodyparaer = require('body-parser')
 
 app = express()
 
+app.set('view engine', 'ejs')
+
 app.use(bodyparaer.urlencoded({
     extended:true
 }))
 
-app.get('/login', function(reg, res) {
-    const options = {
-        root: path.join(__dirname)
-    }
+var con;
 
-    const fileName = 'login.html'
-    
-    res.sendFile(fileName, options, function(err){
-        if(err){
-            runInNewContext(err);
-        } else {
-            console.log('sent : ', fileName)
-        }
-    })
+app.get('/login', function(reg, res) {
+    //HTML Files Path
+    const options = {
+        root: path.join(__dirname,"/../front-end/")
+    }
+    console.log(__dirname);
+
+    const fileName = 'login'
+
+    res.render("login", {login_string: "Please enter Username and password"})
 })
 
 
@@ -31,13 +31,25 @@ app.post("/login", function(req, res){
     var password = req.body.password
 
     console.log(`***\nusername : ${username}\n password : ${password}\n****`)
-
+    //try to login to establish mysql connection
+    con = mysql.createConnection({
+        host: "localhost",
+        user: username,
+        password: password,
+    })
+    con.connect(function(err){
+        if(err){
+            res.render("login", {login_string: "login unsuccesful"})
+        }else{
+            console.log("Connected")
+            res.render("apprprivelage.ejs");
+        } 
+    })
     
+
 })
 
 app.listen(8081, function(err){
     if(err) throw err
     console.log("server is listening at 8081")
 })
-
-
